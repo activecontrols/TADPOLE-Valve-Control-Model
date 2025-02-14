7fe07028c6e4a756265d1167e2e3cf5d03d0e2cc
RobertJN64 <robertjnies@gmail.com>
2025-02-14 14:55:38 -0500

    tuning MFR

--- closed_loop_valve_control_cpp/pi_controller.cpp
+++ closed_loop_valve_control_cpp/pi_controller.cpp
@@ -7,17 +7,11 @@ PI_Controller::PI_Controller(double kp, double ki) {
 
 double PI_Controller::compute(double input_error, double time_delta, double *err_sum) {
   *err_sum += input_error * time_delta;
-  // if (*err_sum > 5) {
-  //   *err_sum = 5;
-  // }
-  // if (*err_sum < -5) {
-  //   *err_sum = -5;
-  // }
   return this->kp * input_error + this->ki * *err_sum;
 }
 
 namespace ClosedLoopControllers {
-PI_Controller Chamber_Pressure_Controller(0.05, 0.2); // TODO RJN CL - PID constants
+PI_Controller Chamber_Pressure_Controller(0.05, 0.2);
 PI_Controller LOX_Angle_Controller(2, 4);
-PI_Controller IPA_Angle_Controller(0, 0);
+PI_Controller IPA_Angle_Controller(2, 4);
 } // namespace ClosedLoopControllers
